8d1e9d25f2c64a958033823a197858335ae2550c
guigui64 <guillaume.comte10@gmail.com>
2019-05-09 09:23:29 +0200

    move components to folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { SigninComponent } from './signin/signin.component';
-import { RegisterComponent } from './register/register.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ImageFormComponent } from './image-form/image-form.component';
+import { AppComponent } from './components/app.component';
+import { MainComponent } from './components/main/main.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ImageFormComponent } from './components/image-form/image-form.component';
 
 @NgModule({
   declarations: [
